072280c71d2047a25b0640201778862696740f3e
sabrina schramme <ichgebesauf@yahoo.de>
2020-03-12 19:37:35 +0100

    Microblog Sabrina Eigene Fotos

---
+++ individuell.js
@@ -0,0 +1,205 @@
+const post = [
+{
+    image: {
+    src: "img/bremen.png",
+    imageAlt: "Bremen"
+  },
+  title: "Sunset Bremen Central Station",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 1
+},
+{
+  image: {
+    src: "img/korfu.png",
+    imageAlt: "Korfu"
+  },
+  title: "Korfu",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 2
+},
+{
+  image: {
+    src: "img/Marokko.png",
+    imageAlt: "Dessert Marokko"
+  },
+  title: "Dessert Marokko",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 3
+},
+{
+  image: {
+    src: "img/Dead Sea.png",
+    imageAlt: "Dead Sea"
+  },
+  title: "Dead Sea",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 4
+},
+{
+  image: {
+    src: "img/Slowenien.png",
+    imageAlt: "Slowenia"
+  },
+  title: "Slowenia",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 5
+},
+{
+  image: {
+    src: "img/muenchen.png",
+    imageAlt: "Munich"
+  },
+  title: "English Garden Munich",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 6
+},
+{
+  image: {
+    src: "img/Lagos.png",
+    imageAlt: "Lagos"
+  },
+  title: "Lagos Beach",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 7
+},
+{
+  image: {
+    src: "img/Lagos Streetart.png",
+    imageAlt: "Lagos"
+  },
+  title: "Lagos Streetart",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 7
+},
+{
+  image: {
+    src: "img/Montenegro_berge.png",
+    imageAlt: "Montenegro"
+  },
+  title: "Mountains Montenegro",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  authorImage: {
+    src: "img/jonathan.jpg"
+  },
+  author: "Sabrina Schramme",
+  date: "März 9, 2020",
+  number: 8
+}
+]
+
+const newContainer = newDiv => newDiv.classList.add ("container", "mx-auto", "max-w-sm", "rounded", "overflow-hidden", "shadow-lg", "justify-center", "bg-white", "m-6")
+
+
+// let newDiv = document.createElement("div");
+
+const newBlogPost = post =>
+  `<img class="w-full" src="${post.image.src}" alt="${post.imageAlt}">
+       <div class="px-6 py-4">
+         <div class="font-bold text-xl mb-2">${post.title}</div>
+         <p class="text-gray-700 text-base">${post.description}</p>
+       </div>
+       <div class="px-6 py-4 flex items-center">
+          <img class="w-10 h-10 rounded-full mr-4" src="${post.authorImage.src}" alt="${post.author}">
+          <div class="text-sm">
+          <p class="text-gray-900 leading-none">${post.author}</p>
+          <p class="text-gray-600">${post.date}</p>
+      </div>
+      </div>
+    </div>`;
+
+for (let i = 0; i < post.length; i++) {
+  const newDiv = document.createElement("div");
+  newContainer (newDiv);
+  newDiv.innerHTML = newBlogPost (post[i]);
+  content.append (newDiv)
+}
+//
+// let newDiv1 = document.createElement("div");
+// newContainer (newDiv1);
+// newDiv1.innerHTML = newBlogPost (post1);
+// content.append (newDiv1);
+//
+// let newDiv2 = document.createElement("div");
+// newContainer (newDiv2);
+// newDiv2.innerHTML = newBlogPost (post2);
+// content.append (newDiv2)
+//
+// let newDiv3 = document.createElement("div");
+// newContainer (newDiv3);
+// newDiv3.innerHTML = newBlogPost (post3);
+// content.append (newDiv3)
+//
+// let newDiv4 = document.createElement("div");
+// newContainer (newDiv4);
+// newDiv4.innerHTML = newBlogPost (post4);
+// content.append (newDiv4)
+//
+// let newDiv5 = document.createElement("div");
+// newContainer (newDiv5);
+// newDiv5.innerHTML = newBlogPost (post5);
+// content.append (newDiv5)
+//
+// let newDiv6 = document.createElement("div");
+// newContainer (newDiv6);
+// newDiv6.innerHTML = newBlogPost (post6);
+// content.append (newDiv6)
+//
+// let newDiv7 = document.createElement("div");
+// newContainer (newDiv7);
+// newDiv7.innerHTML = newBlogPost (post7);
+// content.append (newDiv7)
+//
+// let newDiv8 = document.createElement("div");
+// newContainer (newDiv8);
+// newDiv8.innerHTML = newBlogPost (post8);
+// content.append (newDiv8)
+//
+// let newDiv9 = document.createElement("div");
+// newContainer (newDiv9);
+// newDiv9.innerHTML = newBlogPost (post8);
+// content.append (newDiv9)
+//
+// let newDiv10 = document.createElement("div");
+// newContainer (newDiv10);
+// newDiv10.innerHTML = newBlogPost (post10);
+// content.append (newDiv10)
