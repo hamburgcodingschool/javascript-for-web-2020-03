1418481b42e9a12dc22b0efa7ab5cfe8e02ed504
CarlottaFriedmann <friedmanncarlotta@gmail.com>
2020-03-13 18:30:07 +0100

    neues File individual blog

---
+++ Blog.CF.js
@@ -0,0 +1,165 @@
+const post = [
+    {
+        title: "Akagera National Park",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Akagera National Park Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "1"
+
+    },
+    {
+        title: "Congo-nile-trail",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/congo-nile-trail-34 Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "2"
+
+    },
+    {
+        title: "Kigali",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Kigali Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "3"
+
+    },
+    {
+        title: "lake kivu",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/lake-kivu Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "4"
+
+    },
+    {
+        title: "Musanze",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Musanze Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "5"
+
+    },
+    {
+        title: "Rusizi",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Rusizi Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "6"
+
+    },
+    {
+        title: "Ngoma",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Ngoma Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "7"
+
+    },
+    {
+        title: "Nyanza",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Nyanza Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "8"
+
+    },
+    {
+        title: "Nyungwe National Park",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Nyungwe National Park Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "9"
+
+    },
+    {
+        title: "Volcano National Park",
+        image:  {
+            src: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Volcano National Park Kopie.jpg",
+            alt: "Sunset in the mountains"},
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+        author: "Carlotta Friedmann",
+        author_image: "/Users/carlotta/Desktop/Projekte/Javascript Course/javascript-for-web-2020-03/micro-blog/img/Monster test.png",
+        date: "Jan 18, 2020",
+        number: "10"
+
+    },
+ 
+]
+
+const createPostHtml = post =>
+  `<img class="w-full" src="${post.image.src}" alt="${post.image.alt}">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">${post.title}</div>
+    <p class="text-gray-700 text-base"> 
+    ${post.text}
+    </p>
+  </div>
+  <div class="px-6 py-4 flex items-center">
+    <img class="w-10 h-10 rounded-full mr-4" src="${post.author_image}" alt="${post.author}">
+    <div class="text-sm">
+      <p class="text-gray-900 leading-none">${post.author}</p>
+      <p class="text-gray-600">${post.date}</p>
+    </div>
+  </div>`;
+
+let post1 = post[0]
+let html = createPostHtml(post1);
+
+
+const addClasses = div =>
+  div.classList.add(
+    "container",
+    "mx-auto",
+    "max-w-sm",
+    "rounded",
+    "overflow-hidden",
+    "shadow-lg",
+    "justify-center",
+    "bg-white",
+    "m-6"
+  );
+
+const content = document.getElementById("content");
+
+for (let i = 0; i < post.length; i++) {
+  const div = document.createElement("div");
+  addClasses(div);
+  div.innerHTML = createPostHtml(post[i]);
+  content.append(div);
+}
