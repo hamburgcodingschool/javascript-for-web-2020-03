d0a3a707c5397e75bd96f671589e20372d7b19d5
JakobSchoerle <jakobschoerle@googlemail.com>
2020-03-13 14:48:35 +0100

    Jakob: First Commit

--- micro-blog/index.js
+++ micro-blog/index.js
@@ -1,37 +1,271 @@
 const posts = [
+
+//var post =
+{
+image:
+{
+  src: "img/card-top.jpg",
+  image_alt: "Sunset in the mountains"
+},
+src: "img/card-top.jpg",
+image_alt: "Sunset in the mountains",
+title: "In the Mountains",
+text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
+author: "Firstname Lastname",
+author_image: "img/jonathan.jpg",
+date: "Jan 18, 2020",
+number: 1
+},
+
+//const post2 =
+{
+  image:
   {
-    title: "In The Mountains",
-    image: {
-      src: "img/card-top.jpg",
-      alt: "Sunset in the mountains"
+    src: "img/Amsterdam_Netherlands_Houses_Canal_Night_519772_1920x1080.jpg",
+    image_alt: "Amsterdam"
+  },
+  src: "img/Amsterdam_Netherlands_Houses_Canal_Night_519772_1920x1080.jpg",
+  image_alt: "Amsterdam",
+  title: "Kanäle in Amsterdam",
+  text: "Holland ist die schönste Stadt der Welt",
+  author: "Firstname Lastname",
+  author_image: "img/jonathan.jpg",
+  date: "Mar 11, 2020",
+  number: 1
+},
+
+//  const post3 =
+{
+    image:
+    {
+      src: "img/brüssel.jpg",
+      image_alt: "Brüssel"
     },
-    text:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-    author: "Teresa Holfeld",
-    author_image: "img/teresa-holfeld.jpg",
-    date: "Jan 18, 2020",
+    src: "img/brüssel.jpg",
+    image_alt: "Brüssel",
+    title: "Brüssel",
+    text: "...Das ist Brüssel",
+    author: "Firstname Lastname",
+    author_image: "img/jonathan.jpg",
+    date: "Mar 11, 2020",
     number: 1
   },
+
+  //  const post4 =
   {
-    title: "The Crowded City",
-    image: {
-      src: "img/barcelona.jpeg",
-      alt: "Barcelona"
+      image:
+      {
+        src: "img/prague-wallpaper-1920x1200.jpg",
+        image_alt: "Prag"
+      },
+      src: "img/prague-wallpaper-1920x1200.jpg",
+      image_alt: "Prag",
+      title: "Prag",
+      text: "...Das ist Prag",
+      author: "Firstname Lastname",
+      author_image: "img/jonathan.jpg",
+      date: "Mar 11, 2020",
+      number: 1
     },
-    text:
-      "Barcelona is a very busy city. It is full of tourists, attractions and centuries old buildings. Very beautiful, really.",
-    author: "Teresa Holfeld",
-    author_image: "img/teresa-holfeld.jpg",
-    date: "Feb 28, 2020",
-    number: 2
-  }
+
+  //    const post5 =
+  {
+        image:
+        {
+          src: "img/Budapest_Hungary_Houses_Rivers_Bridges_Night_513925_1920x1080.jpg",
+          image_alt: "Budapest"
+        },
+        src: "img/Budapest_Hungary_Houses_Rivers_Bridges_Night_513925_1920x1080.jpg",
+        image_alt: "Budapest",
+        title: "Budapest",
+        text: "...Das ist Budapest",
+        author: "Firstname Lastname",
+        author_image: "img/jonathan.jpg",
+        date: "Mar 11, 2020",
+        number: 1
+      },
+
+      {
+            image:
+            {
+              src: "img/Royal-Palace-Poland-Warsaw-night_1920x1200.jpg",
+              image_alt: "Warschau"
+            },
+            src: "img/Royal-Palace-Poland-Warsaw-night_1920x1200.jpg",
+            image_alt: "Warschau",
+            title: "Warschau",
+            text: "...Das ist Warschau",
+            author: "Firstname Lastname",
+            author_image: "img/jonathan.jpg",
+            date: "Mar 11, 2020",
+            number: 1
+          },
+
+          {
+                image:
+                {
+                  src: "img/rom.jpg",
+                  image_alt: "Rom"
+                },
+                src: "img/rom.jpg",
+                image_alt: "Rom",
+                title: "Rom",
+                text: "...Das ist Rom",
+                author: "Firstname Lastname",
+                author_image: "img/jonathan.jpg",
+                date: "Mar 11, 2020",
+                number: 1
+              },
+
+              {
+                    image:
+                    {
+                      src: "img/wallpapers-of-paris.jpg",
+                      image_alt: "Paris"
+                    },
+                    src: "img/wallpapers-of-paris.jpg",
+                    image_alt: "Paris",
+                    title: "Paris",
+                    text: "...Das ist Paris",
+                    author: "Firstname Lastname",
+                    author_image: "img/jonathan.jpg",
+                    date: "Mar 11, 2020",
+                    number: 1
+                  },
+                  {
+                        image:
+                        {
+                          src: "img/vatikan.jpg",
+                          image_alt: "Vatikan"
+                        },
+                        src: "img/vatikan.jpg",
+                        image_alt: "vatikan",
+                        title: "Vatikan",
+                        text: "...Das ist der Vatikan",
+                        author: "Firstname Lastname",
+                        author_image: "img/jonathan.jpg",
+                        date: "Mar 11, 2020",
+                        number: 1
+                      },
+
+                      {
+                            image:
+                            {
+                              src: "img/Roads_Houses_Evening_Madrid_Spain_From_above_532163_1920x1080.jpg",
+                              image_alt: "Madrid"
+                            },
+                            src: "img/Roads_Houses_Evening_Madrid_Spain_From_above_532163_1920x1080.jpg",
+                            image_alt: "Madrid",
+                            title: "Madrid",
+                            text: "...Das ist der Madrid",
+                            author: "Firstname Lastname",
+                            author_image: "img/jonathan.jpg",
+                            date: "Mar 11, 2020",
+                            number: 1
+                          }
 ];
+
+/*
+
+let authorName = JSON.stringify(post.author);
+
+
+let content = document.getElementById('content');
+*/
+
+
+// console.log(content);
+
+// Create new element
+
+//let newDiv = document.createElement("div");
+
+//console.log(newDiv);
+
+// Place element in div
+
+//content.append(newDiv);
+
+//newDiv.innerHTML = "HTML hier";
+
+
+
+//newDiv = document.createElement("div");
+
+//let newP = document.createElement("p");
+//newDiv.append(newP);
+
+/*
+
+let newDiv = document.createElement("div");
+
+newDiv.classList.add("container", "mx-auto", "max-w-sm", "rounded", "overflow-hidden", "shadow-lg", "justify-center", "bg-white", "m-6");
+
+newDiv.id = "myDiv";
+
+
+newDiv.innerHTML =`
+  <div class="container mx-auto max-w-sm rounded overflow-hidden shadow-lg justify-center bg-white m-6">
+    <img class="w-full" src="${post.image.src}" alt="${post.image.alt}">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">${post.title}</div>
+      <p class="text-gray-700 text-base">
+        ${post.text}
+    </div>
+    <div class="px-6 py-4 flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="${post.author_image}" alt="Avatar of Jonathan Reinink">
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">${post.author}</p>
+        <p class="text-gray-600">${post.date}</p>
+      </div>
+    </div>
+  </div>`;
+
+content.append(newDiv);
+
+*/
+
+
+// Beginning of second post
+
+//let newDiv2 = document.createElement("div");
+
+// newDiv.classList.add("hier wird eine Klasse hinzugefügt","Hier noch eine Klasse");
+
+/*
+
+newDiv2.id = "myDiv";
+
+newDiv2.classList.add("container", "mx-auto", "max-w-sm", "rounded", "overflow-hidden", "shadow-lg", "justify-center", "bg-white", "m-6");
+
+
+
+newDiv2.innerHTML =`
+  <div class="container mx-auto max-w-sm rounded overflow-hidden shadow-lg justify-center bg-white m-6">
+    <img class="w-full" src="${post2.image.src}" alt="${post2.image.alt}">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">${post2.title}</div>
+      <p class="text-gray-700 text-base">
+        ${post2.text}
+    </div>
+    <div class="px-6 py-4 flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="${post2.author_image}" alt="Avatar of Jonathan Reinink">
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">${post2.author}</p>
+        <p class="text-gray-600">${post2.date}</p>
+      </div>
+    </div>
+  </div>`;
+
+content.append(newDiv2);
+
+*/
 
 const createPostHtml = post =>
   `<img class="w-full" src="${post.image.src}" alt="${post.image.alt}">
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">${post.title}</div>
-    <p class="text-gray-700 text-base"> 
+    <p class="text-gray-700 text-base">
     ${post.text}
     </p>
   </div>
@@ -64,3 +298,47 @@ for (let i = 0; i < posts.length; i++) {
   div.innerHTML = createPostHtml(posts[i]);
   content.append(div);
 }
+/*
+// Post 3
+
+let div3 = document.createElement("div");
+addClasses(div3);
+div3.innerHTML = createBlogPost(post3);
+content.append(div3);
+
+// Post 4
+
+let div4 = document.createElement("div");
+addClasses(div4);
+div4.innerHTML = createBlogPost(post4);
+content.append(div4);
+
+// Post 5
+
+let div5 = document.createElement("div");
+addClasses(div5);
+div5.innerHTML = createBlogPost(post5);
+content.append(div5);
+*/
+/*
+JSON .stringified - macht ein komplettes Objekt zu einem string
+JSON .parse - macht aus dem String wieder ein Objekt
+*/
+
+//document.getElementById('id') --> findet das Element mit der entsprechenden ID
+//document.getElementByClassName('name') --> findet das Element mit dem entsprechenden Klassennamen
+//document.getElementByTagName('tag') --> findet das Element mit dem entsprechenden Tagnamen, zb. h2
+
+/*.querySelectorAll funktioniert wie getElementByX und findet Tags sowie Klassen es ist nicht nur auf Tags, sondern auch auf IDs anwendbar
+zb. content.querySelectorAll("div");
+oder document.querySelectorAll("#id");
+
+fat arrow functions have implicit return
+function aplusb(a, b) = {
+return a+b;
+}
+
+this produces the same result
+const aplusb = (a,b) => a+b;
+
+*/
