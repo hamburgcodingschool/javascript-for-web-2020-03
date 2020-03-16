6cef9801b8a279f5620e7e9b377e8aa9f732703e
Teresa Holfeld <teresa.holfeld@gmail.com>
2020-03-16 13:46:05 +0100

    Take classes from HTML

--- micro-blog/index.js
+++ micro-blog/index.js
@@ -28,39 +28,27 @@ const posts = [
 ];
 
 const createPostHtml = post =>
-  `<img class="w-full" src="${post.image.src}" alt="${post.image.alt}">
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">${post.title}</div>
-    <p class="text-gray-700 text-base"> 
-    ${post.text}
-    </p>
-  </div>
-  <div class="px-6 py-4 flex items-center">
-    <img class="w-10 h-10 rounded-full mr-4" src="${post.author_image}" alt="${post.author}">
-    <div class="text-sm">
-      <p class="text-gray-900 leading-none">${post.author}</p>
-      <p class="text-gray-600">${post.date}</p>
+  `<div class="container mx-auto max-w-sm rounded overflow-hidden shadow-lg justify-center bg-white m-6">
+    <img class="w-full" src="${post.image.src}" alt="${post.image.alt}">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">${post.title}</div>
+      <p class="text-gray-700 text-base"> 
+      ${post.text}
+      </p>
+    </div>
+    <div class="px-6 py-4 flex items-center">
+      <img class="w-10 h-10 rounded-full mr-4" src="${post.author_image}" alt="${post.author}">
+      <div class="text-sm">
+        <p class="text-gray-900 leading-none">${post.author}</p>
+        <p class="text-gray-600">${post.date}</p>
+      </div>
     </div>
   </div>`;
-
-const addClasses = div =>
-  div.classList.add(
-    "container",
-    "mx-auto",
-    "max-w-sm",
-    "rounded",
-    "overflow-hidden",
-    "shadow-lg",
-    "justify-center",
-    "bg-white",
-    "m-6"
-  );
 
 const content = document.getElementById("content");
 
 for (let i = 0; i < posts.length; i++) {
   const div = document.createElement("div");
-  addClasses(div);
   div.innerHTML = createPostHtml(posts[i]);
-  content.append(div);
+  content.append(div.firstChild);
 }
